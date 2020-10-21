02cc732b3e41a968bf671928c5bdfd599166e86c
nakata-0523 <nktstr.20050203@gmail.com>
2020-10-21 22:52:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dentaku/ButtonClickListener.java
@@ -0,0 +1,208 @@
+package com.example.dentaku;
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class ButtonClickListener
+        implements View.OnClickListener {
+
+    final TextView operand1, operator, operand2;
+
+    ButtonClickListener(TextView operand1,
+                        TextView operator,
+                        TextView operand2) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.operator = operator;
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onClick(View v) {
+        final String sOperator = operator.getText().toString();
+        //final String sOperand1 = operand1.getText().toString();
+        //final String sOperand2 = operand2.getText().toString();
+
+        switch ((String) v.getTag()) {
+            case "zero":
+                this.inputOneNumber('0');
+                break;
+            case "one":
+                this.inputOneNumber('1');
+                break;
+            case "two":
+                this.inputOneNumber('2');
+                break;
+            case "three":
+                this.inputOneNumber('3');
+                break;
+            case "four":
+                this.inputOneNumber('4');
+                break;
+            case "five":
+                this.inputOneNumber('5');
+                break;
+            case "six":
+                this.inputOneNumber('6');
+                break;
+            case "seven":
+                this.inputOneNumber('7');
+                break;
+            case "eight":
+                this.inputOneNumber('8');
+                break;
+            case "nine":
+                this.inputOneNumber('9');
+                break;
+            case "dot":
+                this.inputOneNumber('.');
+                break;
+            case "plus":
+                operator.setText("+");
+                break;
+            case "minus":
+                operator.setText("-");
+                break;
+            case "divide":
+                operator.setText("/");
+                break;
+            case "multiply":
+                operator.setText("*");
+                break;
+            case "equal":
+                try {
+                    final double o1 = Double.parseDouble(
+                            operand1.getText().toString());
+                    final double o2 = Double.parseDouble(
+                            operand2.getText().toString());
+                    switch (sOperator) {
+                        case "+":
+                            operatePlus(o1, o2);
+                            break;
+                        case "-":
+                            operateMinus(o1, o2);
+                            break;
+                        case "/":
+                            operateDivide(o1, o2);
+                            break;
+                        case "*":
+                            operateMultiply(o1, o2);
+                            break;
+                    }//switch
+                } catch (NumberFormatException e) {
+                    Toast.makeText(v.getContext(),
+                            e.getMessage(),
+                            Toast.LENGTH_SHORT)
+                            .show();
+                } catch (RuntimeException e) {
+                    Toast.makeText(v.getContext(),
+                            e.getMessage(),
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case "allclear":
+                operand1.setText("");
+                operator.setText("");
+                operand2.setText("");
+                break;
+            case "clear":
+                if (!operand2.getText().toString().contentEquals("")) {
+                    operand2.setText("");
+                } else if (!operator.getText().toString().contentEquals("")) {
+                    operator.setText("");
+                } else if (!operand1.getText().toString().contentEquals("")) {
+                    operand1.setText("");
+                }
+                break;
+            case "delete":
+                if (!operand2.getText().toString().contentEquals("")) {
+                    operand2.setText(
+                            operand2.getText().toString().substring(
+                                    0,
+                                    operand2.getText().length() - 1));
+                } else if (!operator.getText().toString().contentEquals("")) {
+                    operator.setText("");
+                } else if (!operand1.getText().toString().contentEquals("")) {
+                    operand1.setText(
+                            operand1.getText().toString().substring(
+                                    0,
+                                    operand1.getText().length() - 1));
+                }
+                break;
+            case "sign":
+                if (!operand2.getText().toString().contentEquals("")) {
+                    if (operand2.getText().toString().substring(0, 1).contentEquals("-")) {
+                        operand2.setText(operand2.getText().toString().substring(
+                                1,
+                                operand2.getText().toString().length()));
+                    } else {
+                        operand2.setText(String.format(Locale.US,
+                                "%c%s",
+                                '-',
+                                operand2.getText().toString()
+                        ));
+                    }
+                } else if (!operand1.getText().toString().contentEquals("")) {
+                    if (operand1.getText().toString().substring(0, 1).contentEquals("-")) {
+                        operand1.setText(operand1.getText().toString().substring(
+                                1,
+                                operand1.getText().toString().length()));
+                    } else {
+                        operand1.setText(String.format(Locale.US,
+                                "%c%s",
+                                '-',
+                                operand1.getText().toString()
+                        ));
+                    }
+                }
+                break;
+            default:
+        }//switch
+    }//onClick
+
+    private void inputOneNumber(final char i) {
+        if (operator.getText().toString().contentEquals(""))
+            operand1.setText(
+                    String.format(Locale.US,
+                            "%s%c",
+                            operand1.getText().toString(), i));
+        else
+            operand2.setText(
+                    String.format(Locale.US,
+                            "%s%c",
+                            operand2.getText().toString(), i));
+
+    }//inputOneNumber
+
+    private void operatePlus(final double o1, final double o2) {
+        operand1.setText(String.format(Locale.US, "%f", (o1 + o2)));
+        operator.setText("");
+        operand2.setText("");
+    }//operatePlus
+
+    private void operateMinus(final double o1, final double o2) {
+        operand1.setText(String.format(Locale.US, "%f", o1 - o2));
+        operator.setText("");
+        operand2.setText("");
+    }//operateMinus
+
+    private void operateDivide(final double o1, final double o2) {
+        final double result = o1 / o2;
+        if (!Double.isFinite(result)) {
+            throw new RuntimeException("not a finite number");
+        }
+        operand1.setText(String.format(Locale.US, "%f", o1 / o2));
+        operator.setText("");
+        operand2.setText("");
+    }//operateDivide
+
+    private void operateMultiply(final double o1, final double o2) {
+        operand1.setText(String.format(Locale.US, "%f", o1 * o2));
+        operator.setText("");
+        operand2.setText("");
+    }//operateMultiply
+
+}
---
+++ app/src/main/java/com/example/dentaku/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.dentaku;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout buttonPadLinearLayout;
+
+    final String[] buttonTexts =
+            {"AC", "C", "Del", "÷",
+                    "7", "8", "9", "×",
+                    "4", "5", "6", "-",
+                    "1", "2", "3", "+",
+                    "±", "0", ".", "="};
+
+    final String[] buttonTags = {
+            "allclear", "clear", "delete", "divide",
+            "seven", "eight", "nine", "multiply",
+            "four", "five", "six", "minus",
+            "one", "two", "three", "plus",
+            "sign", "zero", "dot", "equal"};
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.buttonPadLinearLayout =
+                findViewById(R.id.buttonPadLinearLayout);
+
+        final ButtonClickListener buttonClickListener =
+                new ButtonClickListener(
+                        (TextView) findViewById(R.id.operand1),
+                        (TextView) findViewById(R.id.operator),
+                        (TextView) findViewById(R.id.operand2)
+                );
+
+        //LinearLayoutとButtonをforループで生成して追加する
+        for (int i = 0; i < 5; ++i) {
+            final LinearLayout newLL = new LinearLayout
+                    (this.getApplicationContext());
+            newLL.setBackgroundColor(Color.YELLOW);
+            newLL.setOrientation(LinearLayout.HORIZONTAL);
+            final ViewGroup.LayoutParams newLP =
+                    new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT);
+            buttonPadLinearLayout.addView(newLL, newLP);
+
+            for (int j = 0; j < 4; ++j) {
+                final Button b = new Button
+                        (this.getApplicationContext());
+                //b.setText(i + "," + j);
+                b.setBackgroundColor(Color.RED);
+                b.setText(buttonTexts[i * 4 + j]);
+                b.setTextSize(22);
+                b.setTextColor(Color.WHITE);
+                //同じイベントリスナで複数のボタンを処理する際にボタンを識別するためタグをつける
+                //各ボタンにIDをつけて識別することもできる
+                b.setTag(buttonTags[i * 4 + j]);
+                b.setOnClickListener(buttonClickListener);
+
+                final LinearLayout.LayoutParams lllp =
+                        new LinearLayout.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT);
+                lllp.bottomMargin = 5;
+                lllp.topMargin = 5;
+                lllp.leftMargin = 5;
+                lllp.rightMargin = 5;
+                newLL.addView(b, lllp);
+            }//for j
+        }//for i
+    }}
